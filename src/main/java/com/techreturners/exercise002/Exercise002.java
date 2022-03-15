2a1621108701737f478e26c099125b11a2c722a8
cliveford <78969930+cliveford@users.noreply.github.com>
2022-03-15 13:54:16 +0000

    Second commit - Exercise 002 completed, & all 002 Tests passed

--- src/main/java/com/techreturners/exercise002/Exercise002.java
+++ src/main/java/com/techreturners/exercise002/Exercise002.java
@@ -1,15 +1,32 @@
 package com.techreturners.exercise002;
 
+import java.util.Objects;
+
+class Person{
+
+    public String firstName;
+    public String lastName;
+    public String town;
+    public int age;
+
+    public Person(String firstName, String lastName,  String town, int age ){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.town = town;
+        this.age = age;
+    }
+}
+
 public class Exercise002 {
 
-    public boolean isFromManchester(/* Person person */) {
+    public boolean isFromManchester(Person person) {
         // Add your code here
-        return false;
+        return Objects.equals(person.town, "Manchester");
     }
 
-    public boolean canWatchFilm(/* Person person, int ageLimit*/) {
+    public boolean canWatchFilm(Person person, int ageLimit) {
         // Add your code here
-        return false;
+        return person.age >= ageLimit;
     }
     
 }
