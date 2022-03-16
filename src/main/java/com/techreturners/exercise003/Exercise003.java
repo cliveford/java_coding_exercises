d7806eb50af9ff25db876a7af9354fe1b06e18d7
cliveford <78969930+cliveford@users.noreply.github.com>
2022-03-16 12:25:33 +0000

    Fourth commit - refactored code for Exercise003 & all Tests passed

--- src/main/java/com/techreturners/exercise003/Exercise003.java
+++ src/main/java/com/techreturners/exercise003/Exercise003.java
@@ -5,12 +5,14 @@ import java.util.Objects;
 public class Exercise003 {
     String[] iceCreams = { "Pistachio", "Raspberry Ripple", "Vanilla", "Mint Chocolate Chip", "Chocolate", "Mango Sorbet" };
     int getIceCreamCode(String iceCreamFlavour) {
+        int iceCreamCode = 0;
         for (int i = 0; i < iceCreams.length; i++) {
             if (Objects.equals(iceCreamFlavour, iceCreams[i])) {
-                return i;
+                iceCreamCode = i;
+                break;
             }
         }
-        throw new UnsupportedOperationException(("Sorry " + iceCreamFlavour + " isn't available"));
+        return iceCreamCode;
     }
 
     String[] iceCreamFlavours() {
