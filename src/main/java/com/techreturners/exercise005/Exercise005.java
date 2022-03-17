745f679c3a0a17cc6f7fe1428f3255b6f1c1d9ca
cliveford <78969930+cliveford@users.noreply.github.com>
2022-03-17 17:40:22 +0000

    Sixth commit - Exercise 005 completed and all Tests passed

--- src/main/java/com/techreturners/exercise005/Exercise005.java
+++ src/main/java/com/techreturners/exercise005/Exercise005.java
@@ -3,7 +3,32 @@ package com.techreturners.exercise005;
 public class Exercise005 {
 
     public boolean isPangram(String input) {
-        throw new UnsupportedOperationException("You can delete this statement and add your code here.");
+        // check if string is empty
+        if (input.length() == 0) return false;
+        // declare and initialise comparison string
+        String compare = "abcdefghijklmnopqrstuvwxyz";
+        // declare and initialise lower case input string
+        String inputLowerCase = input.toLowerCase();
+        // declare and initialise counter to track all letters of alphabet
+        int counter = 0;
+        // loop through alphabet
+        for (int i = 0; i < compare.length(); i++) {
+            // loop through input string
+            for (int j = 0; j < input.length() ; j++) {
+                // check if input string character matches alphabet character
+                if (compare.charAt(i) == inputLowerCase.charAt(j)) {
+                    // match - increment counter and break out of inner loop
+                    counter += 1;
+                    break;
+                }
+            }
+        }
+        // check if all 26 alphabet characters have been matched
+        if (counter == 26) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
